6216e0b6e3841ffc91dda6b671c5f3b4100f058b
licheng <lc900508@sina.com>
2017-08-28 17:09:26 +0800

    commit

--- HnR/Classes/Home/View/LLBabyJudgeCardCell.swift
+++ HnR/Classes/Home/View/LLBabyJudgeCardCell.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class LLBabyJudgeCardCell: UICollectionViewCell {
+    
+    
+    /// 背景遮罩
+    @IBOutlet weak var bgView: UIView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        bgView.backgroundColor = UIColor(colorLiteralRed: 0.0, green: 0.0, blue: 0.0, alpha: 0.3)
     }
 
 }
